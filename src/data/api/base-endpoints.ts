2459d68680929efb7f77ee8f3e33afa7bccac934
Tan Vo <tanvh@trungsonpharma.com>
2025-12-29 08:18:32 +0700

    ELEA-45: change api endpoint for production

--- src/data/api/base-endpoints.ts
+++ src/data/api/base-endpoints.ts
@@ -20,5 +20,5 @@ export const getBaseUrl = (): string => {
     throw new Error('Missing NEXT_PUBLIC_LOCAL_PRODUCTION_BASE_URL environment variable.');
   }
 
-  return local.replace(/\/+$/, '');
+  return production.replace(/\/+$/, '');
 };
